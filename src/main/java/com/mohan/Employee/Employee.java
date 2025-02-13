78596a009b15fc1a1db1074114e8fbb37caf03a1
mohancoder2k <mohanbalu2k5@gmail.com>
2025-02-13 23:41:42 +0530

    first commit

---
+++ src/main/java/com/mohan/Employee/Employee.java
@@ -0,0 +1,30 @@
+package com.mohan.Employee;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String designation;
+    private String phoneNumber;
+    private LocalDate joinedOn;
+    private String address;
+    private LocalDate dateOfBirth;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
